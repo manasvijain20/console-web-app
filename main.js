0698a218db223cfecdd163c581e2507f40c3adce
manasvijain20 <74360258+manasvijain20@users.noreply.github.com>
2021-02-11 17:38:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("This is my first console project")
+var firstname="Manasvi ";
+var lastname="Jain";
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+var x = 24;
+var y = 15;
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
